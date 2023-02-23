e71b2d786aed883a4e7b55c8be72abe454a0b3c2
fmirik <fatihmirik@gmail.com>
2023-02-24 00:06:09 +0300

    v6

--- src/main/java/Class01.java
+++ src/main/java/Class01.java
@@ -3,5 +3,7 @@ public class Class01 {
     public static void main(String[] args) {
 
         System.out.println("1. Calsimam");
+        System.out.println("v3");
+        System.out.println("Bugun hava gunesli");
     }
 }
